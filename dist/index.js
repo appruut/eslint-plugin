67f3b167b35c80b79402ed2761cbc6fcac4a1abc
andrew kariuki <andrew4kariuki@gmail.com>
2021-07-15 02:23:53 +0300

    Typescript Application ESlint plugin

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview Simple ESlint Plugin
+ * @author appruut
+ */
+"use strict";
+
+// sample plugins
+module.exports = {
+  configs: {
+    // tspackages: require("./tspackage.json"),
+    tsapplications: require("./tsapp.json"),
+  },
+};
